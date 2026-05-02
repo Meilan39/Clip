b1ba3e96379dbc9f13743b90106c705b61e57cdc
Meilan39 <meilanmuto2005@gmail.com>
2026-05-02 13:26:25 +0900

    cat and peek formatting.

--- src/data.c
+++ src/data.c
@@ -55,17 +55,19 @@ void cat_data(const Node* node) {
     fseek(f, node->offset, SEEK_SET);
     
     char buffer[4096];
+    char last_char = '\0';
     size_t remaining = node->size;
     while (remaining > 0) {
         size_t to_read = (remaining < sizeof(buffer)) ? remaining : sizeof(buffer);
         size_t bytes = fread(buffer, 1, to_read, f);
         if (bytes == 0) break;
         fwrite(buffer, 1, bytes, stdout);
+        last_char = buffer[bytes - 1];
         remaining -= bytes;
     }
     
     fclose(f);
-    printf("\n");
+    if (last_char != '\n') printf("\n");
 }
 
 void peek_data(const Node* node) {
@@ -80,8 +82,11 @@ void peek_data(const Node* node) {
     buffer[bytes] = '\0';
     
     printf("%s", buffer);
-    if (node->size > 100) printf("...\n");
-    else printf("\n");
+    if (node->size > 100) {
+        printf("...\n");
+    } else if (bytes > 0 && buffer[bytes - 1] != '\n') {
+        printf("\n");
+    }
     
     fclose(f);
 }
