8717f10c24fc70bc44ed5d5541cdf78a3b6a3934
Meilan39 <meilanmuto2005@gmail.com>
2026-05-08 10:33:19 +0900

    data.h mismatch

--- inc/data.h
+++ inc/data.h
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include "block.h"
 
-void move_data(size_t src, size_t dst, size_t size);
-
+void get_data(const Node* node);
 void new_data(const Node* empty, Node* node, const char* segment);
 void cat_data(const Node* node);
 void peek_data(const Node* node);
+void move_data(size_t src, size_t dst, size_t size);
 
 #endif // DATA
